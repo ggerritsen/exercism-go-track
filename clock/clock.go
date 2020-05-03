9b0754fec272b7595443472f34e69017976db93e
ggerritsen <444568+ggerritsen@users.noreply.github.com>
2020-05-03 23:32:06 +0200

    Improved clock

--- clock/clock.go
+++ clock/clock.go
@@ -13,54 +13,29 @@ type Clock interface {
 }
 
 type myClock struct {
-	h, m int
+	m int
 }
 
 func (c myClock) String() string {
-	return fmt.Sprintf("%02d:%02d", c.h, c.m)
+	return fmt.Sprintf("%02d:%02d", c.m/60, c.m%60)
 }
 
 func (c myClock) Add(m int) Clock {
-	return New(c.h, c.m+m)
+	return New(c.m/60, c.m%60+m)
 }
 
 func (c myClock) Subtract(m int) Clock {
-	return New(c.h, c.m-m)
+	return c.Add(-m)
 }
 
 // New creates a new Clock
 func New(h, m int) Clock {
-	// correct minutes >= 60
-	if m >= 60 {
-		h += m / 60
-		m = m % 60
+	mins := h*60 + m
+
+	// remove the days
+	mins %= 24 * 60
+	if mins < 0 {
+		mins += 24 * 60
 	}
-
-	// correct negative minutes
-	if m < 0 {
-		mins := -m
-
-		minHours := mins / 60
-		minMins := mins % 60
-
-		h -= minHours
-		if minMins == 0 {
-			m = 0
-		} else {
-			h--
-			m = 60 - minMins
-		}
-	}
-
-	// correct negative hours
-	if h < 0 {
-		h = 24 + (h%24)
-	}
-
-	// correct hours >= 24
-	if h >= 24 {
-		h = h % 24
-	}
-
-	return myClock{h, m}
+	return myClock{mins}
 }
